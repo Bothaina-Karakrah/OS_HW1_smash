bcf995c12a609e008f264993bfb80cf69a228731
yasmeens16-meet <yasmeen16-meet@github.com>
2020-04-16 15:24:49 +0000

    added Ctrl c+z again

--- signals.cpp
+++ signals.cpp
@@ -7,13 +7,40 @@ using namespace std;
 
 void ctrlZHandler(int sig_num) {
 	// TODO: Add your implementation
+    cout << "smash: got ctrl-Z" << endl;
+    SmallShell &smallShell = smallShell.getInstance();
+    int temp = smallShell.get_curr_pid();
+    //TODO: setup sig alarm handler
+
+    if (temp != -1) {
+        smallShell.get_job_list()->addJob(smallShell.get_job_list()->get_curr_fg_job()->get_cmd() ,true);
+        smallShell.get_job_list()->get_curr_fg_job()->get_cmd()->set_state(Stopped);
+        if (kill(smallShell.get_curr_pid(), SIGSTOP) != 0) {
+            perror("smash error: kill failed");
+        }
+        smallShell.set_curr_pid(-1) ;
+        smallShell.get_job_list()->set_curr_fg_job(nullptr,0);
+        cout << "smash: process " << temp << " was stopped" << endl;
+    }
 }
 
 void ctrlCHandler(int sig_num) {
-  // TODO: Add your implementation
+    // TODO: Add your implementation
+    cout << "smash: got ctrl-C" << endl;
+    SmallShell &smallShell = smallShell.getInstance();
+    int temp = smallShell.get_curr_pid();
+    if (temp != -1) {
+        if (kill(smallShell.get_curr_pid(), SIGKILL) != 0) {
+            perror("smash error: kill failed");
+        }
+        smallShell.set_curr_pid(-1);
+        smallShell.get_job_list()->set_curr_fg_job(nullptr, 0);
+        cout << "smash: process " << temp << " was killed" << endl;
+    }
 }
 
-void alarmHandler(int sig_num) {
-  // TODO: Add your implementation
-}
+
+    void alarmHandler(int sig_num) {
+        // TODO: Add your implementation
+    }
 
