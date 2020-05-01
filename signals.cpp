6ba715848faf8fb99b64ef32d4ac0e07d9516e55
Bothaina-Karakrah <63556283+Bothaina-Karakrah@users.noreply.github.com>
2020-05-01 05:09:58 +0300

    Update signals.cpp

--- signals.cpp
+++ signals.cpp
@@ -7,23 +7,24 @@ using namespace std;
 
 void ctrlZHandler(int sig_num) {
 
-    ///fix stdout to the standard one
-    if (dup2(stdout_fd, 1) == -1) {
-        perror("smash error: dup2 failed");
-    }
+    /fix stdout to the standard one
+   if (dup2(stdout_fd, 1) == -1) {
+       perror("smash error: dup2 failed");
+   }
 
-	// TODO: Add your implementation
+    // TODO: Add your implementation
     cout << "smash: got ctrl-Z" << endl;
     SmallShell &smallShell = smallShell.getInstance();
     int temp = smallShell.get_curr_pid();
     //TODO: setup sig alarm handler
 
     if (temp != -1) {
-        smallShell.get_job_list()->addJob(smallShell.get_job_list()->get_curr_fg_job()->get_cmd() ,true);
-        smallShell.get_job_list()->get_curr_fg_job()->get_cmd()->set_state(Stopped);
         if (kill(smallShell.get_curr_pid(), SIGSTOP) != 0) {
             perror("smash error: kill failed");
+            return;
         }
+        smallShell.get_job_list()->addJob(smallShell.get_job_list()->get_curr_fg_job()->get_cmd() ,true);
+        smallShell.get_job_list()->get_curr_fg_job()->get_cmd()->set_state(Stopped);
         smallShell.set_curr_pid(-1) ;
         smallShell.get_job_list()->set_curr_fg_job(nullptr,0);
         cout << "smash: process " << temp << " was stopped" << endl;
@@ -32,10 +33,10 @@ void ctrlZHandler(int sig_num) {
 
 void ctrlCHandler(int sig_num) {
 
-    ///fix stdout to the standard one
-    if (dup2(stdout_fd, 1) == -1) {
-        perror("smash error: dup2 failed");
-    }
+    /fix stdout to the standard one
+   if (dup2(stdout_fd, 1) == -1) {
+       perror("smash error: dup2 failed");
+   }
 
 
     // TODO: Add your implementation
@@ -45,6 +46,7 @@ void ctrlCHandler(int sig_num) {
     if (temp != -1) {
         if (kill(smallShell.get_curr_pid(), SIGKILL) != 0) {
             perror("smash error: kill failed");
+            return;
         }
         smallShell.set_curr_pid(-1);
         smallShell.get_job_list()->set_curr_fg_job(nullptr, 0);
@@ -53,7 +55,7 @@ void ctrlCHandler(int sig_num) {
 }
 
 
-    void alarmHandler(int sig_num) {
-        // TODO: Add your implementation
-    }
+void alarmHandler(int sig_num) {
+    // TODO: Add your implementation
+}
 
